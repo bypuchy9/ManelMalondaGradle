6482b6e57423b20696994b04a9b1371ffe0a9ee9
Manel <manelmalonda17@gmail.com>
2026-02-13 20:12:28 +0100

    Punto 11: Tareas Ollama arregladas y finalizadas

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
+import org.gradle.api.tasks.Exec
+
 plugins {
     id("java")
-    application
+    id("application")
 }
 
 group = "com.manelmalonda.tema4gradle"
@@ -14,6 +16,8 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    // IA
     implementation(platform("dev.langchain4j:langchain4j-bom:1.10.0"))
     implementation("dev.langchain4j:langchain4j-open-ai")
 }
@@ -21,6 +25,46 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
 application {
     mainClass.set("com.manelmalonda.tema4gradle.Main")
+}
+
+
+val isWindows = System.getProperty("os.name").lowercase().contains("win")
+
+tasks.register<Exec>("ollamaVersion") {
+    group = "ollama"
+    description = "Comprueba la versión de Ollama"
+
+    if (isWindows) {
+        commandLine("powershell", "-c", "ollama --version")
+    } else {
+        commandLine("bash", "-c", "ollama --version")
+    }
+}
+
+tasks.register<Exec>("ollamaPs") {
+    group = "ollama"
+    description = "Muestra modelos en memoria"
+
+    if (isWindows) {
+        commandLine("powershell", "-c", "ollama ps")
+    } else {
+        commandLine("bash", "-c", "ollama ps")
+    }
+}
+
+tasks.register("llmInfo") {
+    group = "ollama"
+    description = "Ejecuta todo el flujo"
+
+    dependsOn("ollamaVersion", "ollamaPs")
+
+    val tPs = tasks.findByName("ollamaPs")
+    tPs?.mustRunAfter("ollamaVersion")
+
+    doLast {
+        println(" Demo finalizada con EXITO")
+    }
 }
